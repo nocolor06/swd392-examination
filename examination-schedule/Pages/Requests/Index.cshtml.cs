7f9f0ab3fc62ffc7fa40e1a92dc885f8928ab0e2
hoenntk68 <hn8319542@gmail.com>
2024-07-11 00:55:27 +0700

    feat: view requests

--- examination-schedule/Pages/Requests/Index.cshtml.cs
+++ examination-schedule/Pages/Requests/Index.cshtml.cs
@@ -11,12 +11,39 @@ namespace examination_schedule.Pages.Requests
 
         public IndexModel(SWD392_ClinicScheduleContext context)
         {
-            context = context;
+            this.context = context;
         }
 
-        public void OnGet()
-        {
+        public IList<Event> Requests { get; set; } = default!;
 
+        public DateTime? From { get; set; }
+
+        public DateTime? To { get; set; }
+
+        public string DoctorId { get; set; }
+
+        public void OnGet(string from, string to, string doctorId)
+        {
+            From = !string.IsNullOrEmpty(from) ? DateTime.Parse(from) : null;
+            To = !string.IsNullOrEmpty(to) ? DateTime.Parse(to) : null;
+            DoctorId = doctorId;
+
+            Requests = context.Events
+            .Include(x => x.Doctor)
+            .ThenInclude(doc => doc.UsernameNavigation)
+            .Include(x => x.Patient)
+            .ThenInclude(pat => pat.UsernameNavigation)
+            .Include(x => x.Staff)
+            .ThenInclude(sta => sta.UsernameNavigation)
+            .Include(x => x.TimeSlot)
+            .Include(x => x.Room)
+            .Where(x =>
+                (From == null || x.Date > From)
+                && (To == null || x.Date < To)
+                && (string.IsNullOrEmpty(DoctorId) || x.DoctorId == DoctorId)
+                && x.Status == "Pending"
+            )
+            .ToList();
         }
     }
 }

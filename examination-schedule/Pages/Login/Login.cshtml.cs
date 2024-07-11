51310f10a5bc21e24451550b046a1b18b4c72fc9
trungnvhs <152192395+trungnvhs@users.noreply.github.com>
2024-07-12 01:33:43 +0700

    Add homepage design and update layout

---
+++ examination-schedule/Pages/Login/Login.cshtml.cs
@@ -0,0 +1,57 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using examination_schedule.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using System.Security.Claims;
+
+namespace examination_schedule.Pages.Login
+{
+    public class LoginModel : PageModel
+    {
+
+
+        private readonly SWD392_ClinicScheduleContext _context;
+
+        public LoginModel(SWD392_ClinicScheduleContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public string Username { get; set; }
+
+        [BindProperty]
+        public string Password { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (ModelState.IsValid)
+            {
+                var user = _context.Users
+                    .FirstOrDefault(u => u.Username == Username && u.Password == Password);
+
+                if (user != null)
+                {
+                    var claims = new List<Claim>
+                    {
+                        new Claim(ClaimTypes.Name, user.Username)
+                        // Add more claims if needed
+                    };
+
+                    var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+                    await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
+
+                    return RedirectToPage("/Homepage/Homepage");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                }
+            }
+
+            return Page();
+        }
+    }
+}

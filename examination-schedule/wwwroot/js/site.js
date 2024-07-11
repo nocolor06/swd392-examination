9e8ce0109d44f5dd100bd009e9da29148a59c5a2
sonnhhe176329 <sonnhhe176329@fpt.edu.vn>
2024-07-12 05:22:13 +0700

    thêm tính năng thêm lịch khám

--- examination-schedule/wwwroot/js/site.js
+++ examination-schedule/wwwroot/js/site.js
@@ -2,3 +2,22 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+body {
+    padding - top: 60px;
+}
+
+.form - control {
+    margin - bottom: 15px;
+}
+
+.needs - validation.form - control:invalid {
+    border - color: #dc3545;
+}
+
+.was - validated.form - control: invalid:focus {
+    box - shadow: 0 0 0 0.2rem rgba(220, 53, 69, 0.25);
+}
+
+.was - validated.form - control: valid:focus {
+    box - shadow: 0 0 0 0.2rem rgba(40, 167, 69, 0.25);
+}

e928d5920efadbeb7e205c42e6ab863785bd11c4
sonnhhe176329 <sonnhhe176329@fpt.edu.vn>
2024-07-17 00:49:16 +0700

    fix search Doctor Schedule

--- examination-schedule/Pages/ScheduleDoctor/Index.cshtml.cs
+++ examination-schedule/Pages/ScheduleDoctor/Index.cshtml.cs
@@ -1,7 +1,9 @@
 using System.Security.Claims;
 using examination_schedule.Models;
+using Humanizer;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace examination_schedule.Pages.TimeTableDoctor
@@ -15,11 +17,18 @@ namespace examination_schedule.Pages.TimeTableDoctor
         {
             _context = context;
         }
+        public IList<Event> Requests { get; set; } = default!;
 
+        public DateTime? From { get; set; }
+
+        public DateTime? To { get; set; }
+
+        public string DoctorId { get; set; }
+        public List<SelectListItem> Doctors { get; set; }
 
         public IList<Event> Events { get; set; } = new List<Event>();
 
-        public IActionResult OnGet()
+        public IActionResult OnGet(string? from, string? to, string? doctorId)
         {
             if(HttpContext.User.Identity == null)
             {
@@ -28,23 +37,51 @@ namespace examination_schedule.Pages.TimeTableDoctor
 
             string username = HttpContext.User.Identity.Name;
 
-            Doctor doctor = _context.Doctors
+            Staff staff = _context.Staff
                 .Where(x => x.Username.Equals(username))
                 .FirstOrDefault();
 
-            if(doctor == null)
+            Doctors = _context.Doctors
+            .Select(d => new SelectListItem
+            {
+                Value = d.DoctorId.ToString(),
+                Text = $"{d.UsernameNavigation.FirstName} {d.UsernameNavigation.LastName}"
+            })
+            .ToList();
+
+            if (staff == null)
             {
                return RedirectToPage("/Login/Index");
             }
 
-            Events = _context.Events
+            /*Events = _context.Events
                     .Include(e => e.Doctor)
                     .Include(e => e.Patient)
                     .Include(e => e.Room)
                     .Include(e => e.Staff)
                     .Include(e => e.TimeSlot)
-                    .Where(e => e.DoctorId == doctor.DoctorId && e.Status.Equals("Approved")) 
-                    .ToList();
+                    .Where(e => e.Status.Equals("Approved")) 
+                    .ToList();*/
+            From = !string.IsNullOrEmpty(from) ? DateTime.Parse(from) : null;
+            To = !string.IsNullOrEmpty(to) ? DateTime.Parse(to) : null;
+            DoctorId = doctorId;
+
+            Events = _context.Events
+            .Include(x => x.Doctor)
+            .ThenInclude(doc => doc.UsernameNavigation)
+            .Include(x => x.Patient)
+            .ThenInclude(pat => pat.UsernameNavigation)
+            .Include(x => x.Staff)
+            .ThenInclude(sta => sta.UsernameNavigation)
+            .Include(x => x.TimeSlot)
+            .Include(x => x.Room)
+            .Where(x =>
+                (From == null || x.Date > From)
+                && (To == null || x.Date < To)
+                && (string.IsNullOrEmpty(DoctorId) || x.DoctorId == DoctorId)
+                && x.Status == "Approved"
+            )
+            .ToList();
             return Page();
         }
     }
